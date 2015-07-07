004d5831c89cb2bc80f7f12220a5054ac159fd16
Lev Kravinsky <kravinskylev@gmail.com>
2015-07-07 17:20:33 -0600

    slug sanitation and default

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,7 @@
 class Restaurant < ActiveRecord::Base
+  before_create :check_slug
   before_create :sanitize_slug
+  validates_presence_of :name, :description
 
   def to_param
     slug
@@ -7,5 +9,9 @@ class Restaurant < ActiveRecord::Base
 
   def sanitize_slug
     self.slug = slug.parameterize
+  end
+
+  def check_slug
+    slug.blank? ? self.slug = name.parameterize : slug
   end
 end
