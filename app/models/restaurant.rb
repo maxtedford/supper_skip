a16ff0ce64c3fb8488b36a7ef6f82d2af6dbc33f
Lev Kravinsky <kravinskylev@gmail.com>
2015-07-07 17:23:40 -0600

    placeholder text and spacing

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,6 +2,7 @@ class Restaurant < ActiveRecord::Base
   before_create :check_slug
   before_create :sanitize_slug
   validates_presence_of :name, :description
+  validates_uniqueness_of :name, :slug
 
   def to_param
     slug
