20e430332ceb5e275ad627d19babe4b360389df6
Lev Kravinsky <kravinskylev@gmail.com>
2015-07-09 17:26:54 -0600

    attempting nested items/restaurants routes

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -11,4 +11,19 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to restaurant_item_path(@items)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:title, :description, :price, :image, :retired, category_ids: [])
+  end
 end
