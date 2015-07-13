f04762186581cd7a4b284ec9c32858f64805e8a0
Max Tedford <max.tedford@gmail.com>
2015-07-13 17:08:37 -0600

    add user_role assignment when user is known

--- app/controllers/user_roles_controller.rb
+++ app/controllers/user_roles_controller.rb
@@ -6,6 +6,23 @@ class UserRolesController < ApplicationController
   end
   
   def create
-    
+    if User.find_by(email_address: params[:user_role][:user_id])
+    @user = User.find_by(email_address: params[:user_role][:user_id])
+    params[:user_role][:user_id] = @user.id.to_s
+    @user_role = UserRole.new(user_role_params)
+      if @user_role.save
+        flash[:message] = "You have successfully added #{@user} as a #{@user.role}"
+        redirect_to restaurant_path(@user_role.restaurant)
+      else
+        flash.now[:errors] = @user_role.errors.full_messages.join(", ")
+        render :new
+      end
+    end
+  end
+  
+  private
+  
+  def user_role_params
+    params.require(:user_role).permit(:user_id, :restaurant_id, :role_id)
   end
 end
--- spec/feature/owner_registers_colleagues_spec.rb
+++ spec/feature/owner_registers_colleagues_spec.rb
@@ -10,7 +10,17 @@ describe 'the application', type: :feature do
         email_address: "viki@example.com",
         password: "password",
         password_confirmation: "password" }
+      cook_data = { name: "Max",
+        email_address: "max@example.com",
+        password: "password",
+        password_confirmation: "password" }
+      delivery_data = { name: "Lev",
+        email_address: "lev@example.com",
+        password: "password",
+        password_confirmation: "password" }
       owner = User.create(owner_data)
+      cook = User.create(cook_data)
+      delivery = User.create(delivery_data)
       owner_role = Role.create(name: "owner")
       cook_role = Role.create(name: "cook")
       delivery_role = Role.create(name: "delivery")
@@ -34,6 +44,17 @@ describe 'the application', type: :feature do
       
       expect(page).to have_content("Email address")
       expect(page).to have_content("User role")
+    end
+    
+    it "adds a new user role when the owner submits the form" do
+      expect(UserRole.all.count).to eq(1)
+      
+      click_link "Add User"
+      fill_in "Email Address", with: "max@example.com"
+      select "cook", :from => "user_role[role_id]"
+      click_button "Register"
+      
+      expect(UserRole.all.count).to eq(2)
     end
   end
 end
