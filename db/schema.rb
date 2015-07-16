069f6fd17e02fe043fa5ada7b713bdf0a9fe3767
androidgrl <kawaharajamie@gmail.com>
2015-07-15 18:23:03 -0600

    Add preptime column

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150715224424) do
+ActiveRecord::Schema.define(version: 20150716000908) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20150715224424) do
     t.datetime "image_updated_at"
     t.boolean  "retired",            default: false
     t.integer  "restaurant_id"
+    t.integer  "prep_time",          default: 12
   end
 
   add_index "items", ["restaurant_id"], name: "index_items_on_restaurant_id", using: :btree
